cf457d969d0905b4c53e3c9c6aaa0449b466aaaf
pla2todkrob <pla2todkrob@gmail.com>
2025-07-02 13:07:18 +0700

    update

--- Multipurpose/Form1.Designer.cs
+++ Multipurpose/Form1.Designer.cs
@@ -28,46 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.windowsSettingsControl1 = new Multipurpose.WindowsSettingsControl();
-            this.windowsUpgradeControl1 = new Multipurpose.WindowsUpgradeControl();
-            this.officeToolsControl1 = new Multipurpose.OfficeToolsControl();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.updateSetApp1 = new Multipurpose.UpdateSetApp();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.windowsUpgradeControl1 = new Multipurpose.WindowsUpgradeControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.windowsSettingsControl1 = new Multipurpose.WindowsSettingsControl();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.officeToolsControl1 = new Multipurpose.OfficeToolsControl();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // windowsSettingsControl1
-            // 
-            this.windowsSettingsControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.windowsSettingsControl1.Location = new System.Drawing.Point(0, 0);
-            this.windowsSettingsControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.windowsSettingsControl1.Name = "windowsSettingsControl1";
-            this.windowsSettingsControl1.Size = new System.Drawing.Size(776, 535);
-            this.windowsSettingsControl1.TabIndex = 0;
-            // 
-            // windowsUpgradeControl1
-            // 
-            this.windowsUpgradeControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.windowsUpgradeControl1.Location = new System.Drawing.Point(3, 3);
-            this.windowsUpgradeControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.windowsUpgradeControl1.Name = "windowsUpgradeControl1";
-            this.windowsUpgradeControl1.Size = new System.Drawing.Size(770, 529);
-            this.windowsUpgradeControl1.TabIndex = 0;
-            // 
-            // officeToolsControl1
-            // 
-            this.officeToolsControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.officeToolsControl1.Location = new System.Drawing.Point(2, 2);
-            this.officeToolsControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.officeToolsControl1.Name = "officeToolsControl1";
-            this.officeToolsControl1.Size = new System.Drawing.Size(772, 531);
-            this.officeToolsControl1.TabIndex = 0;
             // 
             // tabControl1
             // 
@@ -77,65 +52,105 @@
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(784, 561);
+            this.tabControl1.Size = new System.Drawing.Size(1045, 690);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Controls.Add(this.updateSetApp1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(776, 535);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(1037, 661);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "แก้ AKP App";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // updateSetApp1
+            // 
+            this.updateSetApp1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.updateSetApp1.Location = new System.Drawing.Point(4, 4);
+            this.updateSetApp1.Name = "updateSetApp1";
+            this.updateSetApp1.Size = new System.Drawing.Size(1029, 653);
+            this.updateSetApp1.TabIndex = 0;
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.windowsUpgradeControl1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(776, 535);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(1037, 661);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Upgrade Windows";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // windowsUpgradeControl1
+            // 
+            this.windowsUpgradeControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.windowsUpgradeControl1.Location = new System.Drawing.Point(4, 4);
+            this.windowsUpgradeControl1.Margin = new System.Windows.Forms.Padding(5);
+            this.windowsUpgradeControl1.Name = "windowsUpgradeControl1";
+            this.windowsUpgradeControl1.Size = new System.Drawing.Size(1029, 653);
+            this.windowsUpgradeControl1.TabIndex = 0;
+            // 
             // tabPage3
             // 
             this.tabPage3.Controls.Add(this.windowsSettingsControl1);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(776, 535);
+            this.tabPage3.Size = new System.Drawing.Size(1037, 661);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "ตั้งค่า Windows";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
+            // windowsSettingsControl1
+            // 
+            this.windowsSettingsControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.windowsSettingsControl1.Location = new System.Drawing.Point(0, 0);
+            this.windowsSettingsControl1.Margin = new System.Windows.Forms.Padding(5);
+            this.windowsSettingsControl1.Name = "windowsSettingsControl1";
+            this.windowsSettingsControl1.Size = new System.Drawing.Size(1037, 661);
+            this.windowsSettingsControl1.TabIndex = 0;
+            // 
             // tabPage4
             // 
             this.tabPage4.Controls.Add(this.officeToolsControl1);
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabPage4.Size = new System.Drawing.Size(776, 535);
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1037, 661);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Activate Office";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
+            // officeToolsControl1
+            // 
+            this.officeToolsControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.officeToolsControl1.Location = new System.Drawing.Point(3, 3);
+            this.officeToolsControl1.Name = "officeToolsControl1";
+            this.officeToolsControl1.Size = new System.Drawing.Size(1031, 655);
+            this.officeToolsControl1.TabIndex = 0;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1045, 690);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Akkhie Multipurpose Tool";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage4.ResumeLayout(false);
@@ -153,5 +168,6 @@
         private WindowsUpgradeControl windowsUpgradeControl1;
         private System.Windows.Forms.TabPage tabPage4;
         private OfficeToolsControl officeToolsControl1;
+        private UpdateSetApp updateSetApp1;
     }
 }
---
+++ Multipurpose/UpdateSetApp.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Multipurpose
+{
+    public partial class UpdateSetApp : UserControl
+    {
+        // Enum to manage the state of the control
+        private enum AppMode
+        {
+            Idle,
+            Processing
+        }
+
+        private AppMode _currentMode = AppMode.Idle;
+        private Button _activeActionButton = null;
+
+        public UpdateSetApp()
+        {
+            InitializeComponent();
+            this.Load += UpdateSetApp_Load;
+        }
+
+        private void UpdateSetApp_Load(object sender, EventArgs e)
+        {
+            // Initial setup when the control loads
+            SetupInitialState();
+            AssignEventHandlers();
+        }
+
+        /// <summary>
+        /// Sets the initial state of all controls.
+        /// </summary>
+        private void SetupInitialState()
+        {
+            // --- Filters ---
+            grpFilters.Enabled = true;
+            txtQuotationSource.Clear();
+            txtQuotationDest.Clear();
+            txtManifest.Clear();
+            chkUseDateRange.Checked = false;
+            dtpFrom.Value = DateTime.Now;
+            dtpTo.Value = DateTime.Now;
+            panelDateRange.Enabled = false;
+
+            // --- Actions ---
+            grpActions.Enabled = true;
+            flpActions.Enabled = true;
+
+            // --- Results ---
+            dgvResults.DataSource = null;
+            dgvResults.Columns.Clear();
+
+            // --- Process/Cancel Buttons ---
+            panelProcess.Visible = false;
+            btnProcess.Enabled = false;
+            btnProcess.Text = "ดำเนินการ";
+
+            // Reset state variables
+            _currentMode = AppMode.Idle;
+            _activeActionButton = null;
+        }
+
+        /// <summary>
+        /// Assigns click events to all action buttons.
+        /// </summary>
+        private void AssignEventHandlers()
+        {
+            // Filter events
+            chkUseDateRange.CheckedChanged += chkUseDateRange_CheckedChanged;
+
+            // Action button events
+            foreach (Button btn in flpActions.Controls.OfType<Button>())
+            {
+                btn.Click += ActionButton_Click;
+            }
+
+            // Process/Cancel button events
+            btnProcess.Click += btnProcess_Click;
+            btnCancel.Click += btnCancel_Click;
+
+            // DataGridView events
+            dgvResults.CellValueChanged += dgvResults_CellValueChanged;
+            dgvResults.CurrentCellDirtyStateChanged += dgvResults_CurrentCellDirtyStateChanged;
+        }
+
+        /// <summary>
+        /// Handles the click event for any of the main action buttons.
+        /// </summary>
+        private void ActionButton_Click(object sender, EventArgs e)
+        {
+            var clickedButton = sender as Button;
+            if (clickedButton == null) return;
+
+            // --- Enter Processing Mode ---
+            _currentMode = AppMode.Processing;
+            _activeActionButton = clickedButton;
+
+            // Update UI for processing mode
+            grpFilters.Enabled = false;
+            flpActions.Enabled = false; // Disable the whole panel of actions
+            panelProcess.Visible = true;
+            btnProcess.Text = clickedButton.Text; // Set the process button text to the action name
+
+            // --- Search Phase ---
+            // This is where you would run the specific query based on the button clicked.
+            // For now, we'll just simulate it with mock data.
+            SearchForData(clickedButton.Name);
+        }
+
+        /// <summary>
+        /// Simulates searching for data and populating the DataGridView.
+        /// </summary>
+        private void SearchForData(string actionName)
+        {
+            // TODO: Replace this with your actual database query logic.
+            // You can use a switch statement on `actionName` (e.g., "btnUnlockQuotation")
+            // to call the correct search method.
+
+            // Example:
+            // switch (actionName)
+            // {
+            //     case "btnUnlockQuotation":
+            //         dgvResults.DataSource = YourDataAccess.SearchUnlockableQuotations(...);
+            //         break;
+            //     // ... other cases
+            // }
+
+            // Mockup Data for demonstration
+            var dt = new DataTable();
+            dt.Columns.Add("Select", typeof(bool));
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Description", typeof(string));
+            dt.Columns.Add("Status", typeof(string));
+
+            for (int i = 1; i <= 10; i++)
+            {
+                dt.Rows.Add(false, i, $"ผลลัพธ์จาก '{actionName}' รายการที่ {i}", "Pending");
+            }
+
+            dgvResults.DataSource = dt;
+            dgvResults.Columns["ID"].ReadOnly = true;
+            dgvResults.Columns["Description"].ReadOnly = true;
+            dgvResults.Columns["Status"].ReadOnly = true;
+            dgvResults.Columns["Description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        /// <summary>
+        /// Handles the "ดำเนินการ" button click.
+        /// </summary>
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (_activeActionButton == null) return;
+
+            // Get selected items from DataGridView
+            var selectedIds = new List<int>();
+            foreach (DataGridViewRow row in dgvResults.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["Select"].Value))
+                {
+                    selectedIds.Add(Convert.ToInt32(row.Cells["ID"].Value));
+                }
+            }
+
+            if (!selectedIds.Any())
+            {
+                MessageBox.Show("กรุณาเลือกอย่างน้อย 1 รายการเพื่อดำเนินการ", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // --- Execution Phase ---
+            // TODO: Add your specific logic here based on the active action button.
+            // You can use a switch on `_activeActionButton.Name`.
+
+            MessageBox.Show($"กำลังดำเนินการ '{_activeActionButton.Text}' กับ {selectedIds.Count} รายการที่เลือก.", "Processing", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // For example:
+            // switch (_activeActionButton.Name)
+            // {
+            //     case "btnUnlockQuotation":
+            //         YourDataAccess.UnlockQuotations(selectedIds);
+            //         break;
+            //     // ... other cases
+            // }
+
+            // --- Reset to initial state after processing ---
+            SetupInitialState();
+        }
+
+        /// <summary>
+        /// Handles the "ยกเลิก" button click.
+        /// </summary>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            SetupInitialState();
+        }
+
+        #region Control and DataGridView Event Handlers
+
+        private void chkUseDateRange_CheckedChanged(object sender, EventArgs e)
+        {
+            panelDateRange.Enabled = chkUseDateRange.Checked;
+        }
+
+        // This is necessary to make the checkbox click register immediately.
+        private void dgvResults_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dgvResults.IsCurrentCellDirty)
+            {
+                dgvResults.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            }
+        }
+
+        // After a checkbox value changes, check if we should enable the Process button.
+        private void dgvResults_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (_currentMode == AppMode.Processing && e.ColumnIndex == dgvResults.Columns["Select"].Index)
+            {
+                UpdateProcessButtonState();
+            }
+        }
+
+        /// <summary>
+        /// Checks all rows and enables the Process button if at least one is selected.
+        /// </summary>
+        private void UpdateProcessButtonState()
+        {
+            bool anySelected = false;
+            foreach (DataGridViewRow row in dgvResults.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["Select"].Value))
+                {
+                    anySelected = true;
+                    break;
+                }
+            }
+            btnProcess.Enabled = anySelected;
+        }
+
+        #endregion
+    }
+}
